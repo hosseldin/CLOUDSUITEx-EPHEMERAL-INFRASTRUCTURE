38b0dca65a8c6b821f0b50e56fa1cc51f300b0a3
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-10-05 16:09:26 +0300

    Setting up the backend for remote state managment

---
+++ terraform/backend.tf
@@ -0,0 +1,19 @@
+# ==============================================
+#
+# =========== TERRAFORM FILE START =============
+#
+# ==============================================
+#
+# Created By: Hossam Mahmoud
+# Date: 2025-10-05
+# Filename: backend.tf
+# Description: Terraform backend configuration for storing state in Azure Blob Storage.
+# Version: 1.0.0
+# Copyright (c) 2025 Hossam. All rights reserved.
+#
+# ==============================================
+#
+# ====== TERRAFORM IMPLEMENTATION START ======
+#
+# ==============================================
+
